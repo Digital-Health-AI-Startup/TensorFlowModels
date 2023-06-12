32b2fe2259513a811875e912532539e3b3f4e3d3
PercipioCorey <corey@percipiohealth.com>
2023-06-12 11:38:32 -0400

    initial commit

--- official/vision/configs/common.py
+++ official/vision/configs/common.py
@@ -40,6 +40,14 @@ class TfExampleDecoderLabelMap(hyperparams.Config):
 
 
 @dataclasses.dataclass
+class ViaDecoder(hyperparams.Config):
+  """A simple TF Example decoder config."""
+  regenerate_source_id: bool = False
+  mask_binarize_threshold: Optional[float] = None
+  attribute_names: List[str] = dataclasses.field(default_factory=list)
+
+
+@dataclasses.dataclass
 class DataDecoder(hyperparams.OneOfConfig):
   """Data decoder config.
 
@@ -47,10 +55,12 @@ class DataDecoder(hyperparams.OneOfConfig):
     type: 'str', type of data decoder be used, one of the fields below.
     simple_decoder: simple TF Example decoder config.
     label_map_decoder: TF Example decoder with label map config.
+    via_decoder: VGG Image Annotator JSON format decoder
   """
   type: Optional[str] = 'simple_decoder'
   simple_decoder: TfExampleDecoder = TfExampleDecoder()
   label_map_decoder: TfExampleDecoderLabelMap = TfExampleDecoderLabelMap()
+  via_decoder: ViaDecoder = ViaDecoder()
 
 
 @dataclasses.dataclass
